b386984c83e7d745cefe83e235cf2f0dd7b35185
gaborv <gabor.varga2@logmein.com>
2016-11-28 15:58:56 +0100

    Initial commit

---
+++ src/Debouncer.elm
@@ -0,0 +1,124 @@
+module Debouncer
+    exposing
+        ( DebouncerState
+        , BounceDetails
+        , SelfMsg
+        , create
+        , bounce
+        , process
+        )
+
+{-|
+
+This is a delay-based debouncer, where given a _minimum_ delay and an action
+to issue, we'll build a stateful component that will eventually issue the action
+_once_, after being given a slew of requests within the delay timeframe.
+
+
+## Debouncer State
+
+@docs DebouncerState
+@docs create
+
+
+## Starting and bouncing the Debouncer
+
+@docs BounceDetails
+@docs bounce
+
+
+## Internal message handling
+
+@docs SelfMsg
+@docs process
+
+-}
+
+import Time exposing (Time)
+import Task exposing (Task)
+import Process
+import Dict exposing (Dict)
+
+
+type alias Id =
+    String
+
+
+{-| The state of tagged debouncers
+-}
+type DebouncerState
+    = DebouncerState
+        { delayTime : Time
+        , pendingMessages : Dict Id Int
+        }
+
+
+{-| Pass these details to the bounce function to start / bounce the debouncer
+-}
+type alias BounceDetails a =
+    { id : Id
+    , msgToSend : a
+    }
+
+
+{-| Create tagged debouncers with a given delay time (shared accross all of them)
+-}
+create : Time -> DebouncerState
+create delayTime =
+    DebouncerState
+        { delayTime = delayTime
+        , pendingMessages = Dict.empty
+        }
+
+
+{-| Forward SelfMsgs to the process function
+-}
+type SelfMsg a
+    = DelayExpired Id a
+
+
+{-| Call bounce to start the debouncer as well as anytime the desired outccome changes.
+-}
+bounce : BounceDetails a -> DebouncerState -> ( DebouncerState, Cmd (SelfMsg a) )
+bounce { id, msgToSend } (DebouncerState currentState) =
+    let
+        counterInc =
+            Maybe.map ((+) 1) >> Maybe.withDefault 1 >> Just
+
+        --(currentState.pendingMessages |> Dict.get id |> Maybe.withDefault 0) + 1
+        delayedCmd =
+            Task.perform
+                (always (DelayExpired id msgToSend))
+                (Process.sleep currentState.delayTime)
+
+        updatedState =
+            { currentState
+                | pendingMessages = currentState.pendingMessages |> Dict.update id counterInc
+            }
+    in
+        ( DebouncerState updatedState, delayedCmd )
+
+
+{-| Forward the debouncer's `SelfMsg`s to this function so that debouncer can manage its state and fire the delayed commands when it is neccessary
+-}
+process : SelfMsg a -> DebouncerState -> ( DebouncerState, Cmd a )
+process (DelayExpired id msg) (DebouncerState state) =
+    let
+        remainingMessages =
+            (state.pendingMessages |> Dict.get id |> Maybe.withDefault 0) - 1
+
+    in
+        if remainingMessages == 0 then
+            DebouncerState
+                { state
+                    | pendingMessages = state.pendingMessages |> Dict.remove id
+                }
+                ! [ (Task.perform identity (Task.succeed msg)) ]
+        else if remainingMessages > 0 then
+            DebouncerState
+                { state
+                    | pendingMessages = state.pendingMessages |> Dict.insert id remainingMessages
+                }
+                ! []
+        else
+            Debug.crash "Invalid debouncer state: there was no state informtion for the supplied Id."
